59e617a0fc4c21075563404ba6499bc309622a25
yeon-a-choi <banmu_@naver.com>
2021-04-15 12:50:42 +0900

    board notice(JUnit Test 중)
    board notice(JUnit Test 중)

--- src/main/java/com/shs/s1/board/notice/NoticeDAO.java
+++ src/main/java/com/shs/s1/board/notice/NoticeDAO.java
@@ -1,5 +1,44 @@
 package com.shs.s1.board.notice;
 
-public class NoticeDAO {
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.shs.s1.board.BoardDAO;
+import com.shs.s1.board.BoardDTO;
+
+@Repository
+public class NoticeDAO implements BoardDAO{
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	private String NAMESPACE = "com.shs.s1.board.notice.NoticeDAO.";
+
+	
+	@Override
+	public List<BoardDTO> getList(BoardDTO boardDTO) throws Exception {
+		return sqlSession.selectList(NAMESPACE+"getList", boardDTO);
+	}
+
+	@Override
+	public int setInsert(BoardDTO boardDTO) throws Exception {
+		return sqlSession.insert(NAMESPACE+"setInsert", boardDTO);
+	}
+
+	@Override
+	public int setUpdate(BoardDTO boardDTO) throws Exception {
+		return sqlSession.update(NAMESPACE+"setUpdate", boardDTO);
+	}
+
+	@Override
+	public int setDelete(BoardDTO boardDTO) throws Exception {
+		return sqlSession.delete(NAMESPACE+"setDelete", boardDTO);
+	}
+	
+	
+	
 
 }
---
+++ src/test/java/com/shs/s1/board/NoticeDAOTest.java
@@ -0,0 +1,13 @@
+package com.shs.s1.board;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NoticeDAOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
