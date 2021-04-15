830a60569182390d38b71d5e1536e990a8de541d
yeon-a-choi <banmu_@naver.com>
2021-04-15 19:34:10 +0900

    board - notice
    board - notice (getList까지)

--- src/main/java/com/shs/s1/board/notice/NoticeController.java
+++ src/main/java/com/shs/s1/board/notice/NoticeController.java
@@ -1,5 +1,34 @@
 package com.shs.s1.board.notice;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shs.s1.board.BoardDTO;
+
+@Controller
+@RequestMapping("/notice/**")
 public class NoticeController {
 
+	@Autowired
+	private NoticeService noticeService;
+	
+	@GetMapping("noticeList")
+	public ModelAndView getList(BoardDTO boardDTO) throws Exception{
+		
+		ModelAndView mv = new ModelAndView();
+		
+		List<BoardDTO> ar = noticeService.getList(boardDTO);
+		
+		mv.addObject("list", ar);
+		mv.addObject("board", "NOTICE");
+		mv.setViewName("board/boardList");
+		
+		return mv;
+		
+	}
 }
--- src/main/java/com/shs/s1/board/notice/NoticeService.java
+++ src/main/java/com/shs/s1/board/notice/NoticeService.java
@@ -1,5 +1,44 @@
 package com.shs.s1.board.notice;
 
-public class NoticeService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.shs.s1.board.BoardDTO;
+import com.shs.s1.board.BoardService;
+
+@Service
+public class NoticeService implements BoardService{
+
+	@Autowired
+	private NoticeDAO noticeDAO; 
+	
+	@Override
+	public List<BoardDTO> getList(BoardDTO boardDTO) throws Exception {
+		return noticeDAO.getList(boardDTO);
+	}
+
+	@Override
+	public int setInsert(BoardDTO boardDTO) throws Exception {
+		return 0;
+	}
+
+	@Override
+	public int getSelect(BoardDTO boardDTO) throws Exception {
+		return 0;
+	}
+
+	@Override
+	public int setUpdate(BoardDTO boardDTO) throws Exception {
+		return 0;
+	}
+
+	@Override
+	public int setDelect(BoardDTO boardDTO) throws Exception {
+		return 0;
+	}
+	
+	
 
 }
