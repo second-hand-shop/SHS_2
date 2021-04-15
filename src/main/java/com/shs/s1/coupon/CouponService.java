99ec0810dd3d8fc2c974190e10d6da0fe4e7102a
HyungsooLim <hyungsoolim0@gmail.com>
2021-04-15 16:40:37 +0900

    coupon_insert_1

---
+++ src/main/java/com/shs/s1/coupon/CouponService.java
@@ -0,0 +1,23 @@
+package com.shs.s1.coupon;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CouponService {
+
+	@Autowired
+	private CouponDAO couponDAO;
+//---------------------------------------------------------------------------------------
+	// getList ===================================================	
+	public List<CouponDTO> getList() throws Exception {
+		return couponDAO.getList();
+	}
+	
+	// setInsert ===================================================
+	public int setInsert(CouponDTO couponDTO) throws Exception {
+		return couponDAO.setInsert(couponDTO);
+	}
+}
