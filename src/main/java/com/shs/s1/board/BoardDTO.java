5085aa3ddc452ea6d2be2d982cafcd86f05e5d6a
yeon-a-choi <banmu_@naver.com>
2021-04-15 11:13:52 +0900

    board-notice
    board-notice (mapper, dto)

---
+++ src/main/java/com/shs/s1/board/BoardDTO.java
@@ -0,0 +1,54 @@
+package com.shs.s1.board;
+
+import java.sql.Date;
+
+public class BoardDTO {
+	
+	private long num;
+	private String title;
+	private String writer;
+	private String contents;
+	private Date regdate;
+	private long hit;
+	
+	
+	//get set
+	public long getNum() {
+		return num;
+	}
+	public void setNum(long num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	public long getHit() {
+		return hit;
+	}
+	public void setHit(long hit) {
+		this.hit = hit;
+	}
+	
+
+}
