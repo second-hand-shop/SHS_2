ff20bc391b8727197855df6623cb28e5823e65cc
wook95 <wook@iugchang-ui-MacBookPro.local>
2021-04-17 13:52:20 +0900

    pom.xml에 썸네일 관련 의존성 추가

---
+++ src/main/java/com/shs/s1/productImage/ProductImageDTO.java
@@ -0,0 +1,14 @@
+package com.shs.s1.productImage;
+
+public class ProductImageDTO {
+
+	
+	private long imageNum;
+	private long productNum;
+	private String thumbNail;
+	private String contents;
+	private String fileName;
+	private String ogName;
+	
+	
+}
