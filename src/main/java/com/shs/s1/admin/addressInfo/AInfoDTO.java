98521577f6c49e2741888337da284e927828cf95
HyungsooLim <hyungsoolim0@gmail.com>
2021-04-29 00:14:21 +0900

    member_addressView_2
    making package

---
+++ src/main/java/com/shs/s1/admin/addressInfo/AInfoDTO.java
@@ -0,0 +1,11 @@
+package com.shs.s1.admin.addressInfo;
+
+public class AInfoDTO {
+	private long addressNum;
+	private String id;
+	private long zipCode;
+	private String majorAddr;
+	private String minorAddr;
+	private String addrMessage;
+	private long orderNum;
+}
